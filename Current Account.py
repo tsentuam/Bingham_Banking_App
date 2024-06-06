b16fcb530ddddbca579b1212f67396305929d5df
Oswald1033 <150370671+Oswald1033@users.noreply.github.com>
2024-06-06 16:04:55 +0100

    Create Current Account.py
    Current account with no restrictions

---
+++ Current Account.py
@@ -0,0 +1,12 @@
+##Project By:
+#Oswald Adoste Edeoja  | BHU/23/04/05/0058
+#Current account with no restrictions
+
+
+from main import Account
+
+class CurrentAccount(Account):
+    pass
+
+CurrentBalance = CurrentAccount()
+CurrentBalance.deposit(1000000)
